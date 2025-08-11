6ae27a9cda573725cde4b22c4ff27c19006e0c6b
zhangkai <chinazhangk@gmail.com>
2025-08-11 17:54:51 +0800

    feat:更新序列化相关测试用例

--- src/test/java/top/kgame/lib/test/snapshot/ComponentSerializeTest.java
+++ src/test/java/top/kgame/lib/test/snapshot/ComponentSerializeTest.java
@@ -8,9 +8,13 @@ import top.kgame.lib.snapshot.tools.ReplicatedWriter;
 import top.kgame.lib.test.snapshot.struct.TestSyncComponent;
 import top.kgame.lib.test.snapshot.struct.TestSyncStruct;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 public class ComponentSerializeTest {
     @Test
-    public void test() {
+    public void testNormalValues() {
         TestSyncComponent encodeComponent = new TestSyncComponent();
         
         // 设置所有基本类型变量
@@ -39,6 +43,28 @@ public class ComponentSerializeTest {
         encodeStruct.setSs("嵌套结构测试");
         
         encodeComponent.setStruct(encodeStruct);
+        
+        // 设置List类型
+        encodeComponent.setListObj(Arrays.asList(encodeStruct));
+        encodeComponent.setListBoolean(Arrays.asList(true, false, true));
+        encodeComponent.setListByte(Arrays.asList((byte) 1, (byte) 2, (byte) 3));
+        encodeComponent.setListChar(Arrays.asList('A', 'B', '中'));
+        encodeComponent.setListShort(Arrays.asList((short) 100, (short) 200));
+        encodeComponent.setListInt(Arrays.asList(1000, 2000, 3000));
+        encodeComponent.setListLong(Arrays.asList(10000L, 20000L));
+        encodeComponent.setListFloat(Arrays.asList(1.1f, 2.2f, 3.3f));
+        encodeComponent.setListDouble(Arrays.asList(1.11, 2.22, 3.33));
+        encodeComponent.setListString(Arrays.asList("字符串1", "字符串2", "字符串3"));
+        
+        // 设置数组类型
+        encodeComponent.setBooleanArray(new boolean[]{true, false, true, false});
+        encodeComponent.setCharArray(new char[]{'X', 'Y', 'Z', '测'});
+        encodeComponent.setShortArray(new short[]{300, 400, 500});
+        encodeComponent.setIntArray(new int[]{4000, 5000, 6000});
+        encodeComponent.setLongArray(new long[]{40000L, 50000L, 60000L});
+        encodeComponent.setFloatArray(new float[]{4.4f, 5.5f, 6.6f});
+        encodeComponent.setDoubleArray(new double[]{4.44, 5.55, 6.66});
+        encodeComponent.setStringArray(new String[]{"数组1", "数组2", "数组3"});
 
         ReplicatedWriter replicatedWriter = ReplicatedWriter.getInstance();
         encodeComponent.serialize(replicatedWriter);
@@ -51,14 +77,18 @@ public class ComponentSerializeTest {
         decodeComponent.deserialize(replicatedReader);
 
         assert encodeComponent.equals(decodeComponent);
+        replicatedWriter.reset();
+        
+        System.out.println("testNormalValues 测试通过！");
     }
 
     @Test
-    public void test1() {
+    public void testMinValuesAndNulls() {
         TestSyncComponent encodeComponent = new TestSyncComponent();
-        // 设置所有基本类型变量
+        
+        // 设置所有基本类型变量为最小值
         encodeComponent.setB(Byte.MIN_VALUE);
-        encodeComponent.setC('？');
+        encodeComponent.setC('\u0000');
         encodeComponent.setBl(false);
         encodeComponent.setInt16(Short.MIN_VALUE);
         encodeComponent.setInt32(Integer.MIN_VALUE);
@@ -66,9 +96,30 @@ public class ComponentSerializeTest {
         encodeComponent.setF32(Float.MIN_VALUE);
         encodeComponent.setF64(Double.MIN_VALUE);
         encodeComponent.setBs(null);
-        encodeComponent.setSs("null");
-
+        encodeComponent.setSs(null);
         encodeComponent.setStruct(null);
+
+        // 设置List类型为null或空
+        encodeComponent.setListObj(null);
+        encodeComponent.setListBoolean(new ArrayList<>());
+        encodeComponent.setListByte(null);
+        encodeComponent.setListChar(new ArrayList<>());
+        encodeComponent.setListShort(null);
+        encodeComponent.setListInt(new ArrayList<>());
+        encodeComponent.setListLong(null);
+        encodeComponent.setListFloat(new ArrayList<>());
+        encodeComponent.setListDouble(null);
+        encodeComponent.setListString(Arrays.asList(null, "", null));
+        
+        // 设置数组类型为null或空
+        encodeComponent.setBooleanArray(null);
+        encodeComponent.setCharArray(new char[0]);
+        encodeComponent.setShortArray(null);
+        encodeComponent.setIntArray(new int[0]);
+        encodeComponent.setLongArray(null);
+        encodeComponent.setFloatArray(new float[0]);
+        encodeComponent.setDoubleArray(null);
+        encodeComponent.setStringArray(new String[]{null, null});
 
         ReplicatedWriter replicatedWriter = ReplicatedWriter.getInstance();
         encodeComponent.serialize(replicatedWriter);
@@ -81,38 +132,75 @@ public class ComponentSerializeTest {
         decodeComponent.deserialize(replicatedReader);
 
         assert encodeComponent.equals(decodeComponent);
+        replicatedWriter.reset();
+        
+        System.out.println("testMinValuesAndNulls 测试通过！");
     }
 
     @Test
-    public void test3() {
+    public void testMaxValuesAndSpecialCases() {
         TestSyncComponent encodeComponent = new TestSyncComponent();
 
-        // 设置所有基本类型变量
+        // 设置所有基本类型变量为最大值和特殊值
         encodeComponent.setB(Byte.MAX_VALUE);
-        encodeComponent.setC('A');
+        encodeComponent.setC('\uFFFF');
         encodeComponent.setBl(true);
         encodeComponent.setInt16(Short.MAX_VALUE);
         encodeComponent.setInt32(Integer.MAX_VALUE);
-        encodeComponent.setInt64(Long.MIN_VALUE);
+        encodeComponent.setInt64(Long.MAX_VALUE);
         encodeComponent.setF32(Float.MAX_VALUE);
         encodeComponent.setF64(Double.MAX_VALUE);
-        encodeComponent.setBs(new byte[]{1, 2, 3, 4, 5});
-        encodeComponent.setSs("测试字符串");
+        encodeComponent.setBs(new byte[]{Byte.MIN_VALUE, Byte.MAX_VALUE, 0});
+        encodeComponent.setSs("🌟特殊字符测试🚀\n\t换行制表符");
 
         // 创建并设置嵌套的TestSyncStruct对象
         TestSyncStruct encodeStruct = new TestSyncStruct();
         encodeStruct.setB((byte) 0);
-        encodeStruct.setC('B');
-        encodeStruct.setBl(false);
+        encodeStruct.setC('中');
+        encodeStruct.setBl(true);
         encodeStruct.setInt16((short) 0);
         encodeStruct.setInt32(0);
         encodeStruct.setInt64(0);
-        encodeStruct.setF32(0f);
-        encodeStruct.setF64(0);
-        encodeStruct.setBs(new byte[]{6, 7, 8, 9, 10});
-        encodeStruct.setSs("嵌套结构测试");
+        encodeStruct.setF32(Float.NaN);
+        encodeStruct.setF64(Double.POSITIVE_INFINITY);
+        encodeStruct.setBs(new byte[0]);
+        encodeStruct.setSs("");
 
         encodeComponent.setStruct(encodeStruct);
+        
+        // 设置List类型为包含各种特殊值
+        TestSyncStruct struct2 = new TestSyncStruct();
+        struct2.setB((byte) -1);
+        struct2.setC('Ω');
+        struct2.setBl(false);
+        struct2.setInt16((short) -32768);
+        struct2.setInt32(-1);
+        struct2.setInt64(-1L);
+        struct2.setF32(Float.NEGATIVE_INFINITY);
+        struct2.setF64(Double.NaN);
+        struct2.setBs(new byte[]{-128, 127});
+        struct2.setSs("特殊struct");
+        
+        encodeComponent.setListObj(Arrays.asList(encodeStruct, struct2, null));
+        encodeComponent.setListBoolean(Arrays.asList(true, true, false, false, true));
+        encodeComponent.setListByte(Arrays.asList(Byte.MIN_VALUE, (byte) 0, Byte.MAX_VALUE));
+        encodeComponent.setListChar(Arrays.asList('\u0000', 'A', '\uFFFF', '中'));
+        encodeComponent.setListShort(Arrays.asList(Short.MIN_VALUE, (short) 0, Short.MAX_VALUE));
+        encodeComponent.setListInt(Arrays.asList(Integer.MIN_VALUE, 0, Integer.MAX_VALUE));
+        encodeComponent.setListLong(Arrays.asList(Long.MIN_VALUE, 0L, Long.MAX_VALUE));
+        encodeComponent.setListFloat(Arrays.asList(Float.MIN_VALUE, 0.0f, Float.MAX_VALUE, Float.NaN, Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY));
+        encodeComponent.setListDouble(Arrays.asList(Double.MIN_VALUE, 0.0, Double.MAX_VALUE, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY));
+        encodeComponent.setListString(Arrays.asList("", "正常字符串", null, "🌟特殊符号🚀", "\n换行\t制表符"));
+        
+        // 设置数组类型为包含各种边界值
+        encodeComponent.setBooleanArray(new boolean[]{true, false});
+        encodeComponent.setCharArray(new char[]{'\u0000', 'Z', '\uFFFF'});
+        encodeComponent.setShortArray(new short[]{Short.MIN_VALUE, 0, Short.MAX_VALUE});
+        encodeComponent.setIntArray(new int[]{Integer.MIN_VALUE, -1, 0, 1, Integer.MAX_VALUE});
+        encodeComponent.setLongArray(new long[]{Long.MIN_VALUE, Long.MAX_VALUE});
+        encodeComponent.setFloatArray(new float[]{Float.MIN_VALUE, Float.MAX_VALUE, Float.NaN, Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY});
+        encodeComponent.setDoubleArray(new double[]{Double.MIN_VALUE, Double.MAX_VALUE, Double.NaN, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY});
+        encodeComponent.setStringArray(new String[]{"边界测试", "", null, "🎯Unicode测试🌟"});
 
         ReplicatedWriter replicatedWriter = ReplicatedWriter.getInstance();
         encodeComponent.serialize(replicatedWriter);
@@ -125,5 +213,8 @@ public class ComponentSerializeTest {
         decodeComponent.deserialize(replicatedReader);
 
         assert encodeComponent.equals(decodeComponent);
+        replicatedWriter.reset();
+        
+        System.out.println("testMaxValuesAndSpecialCases 测试通过！");
     }
 }
--- src/test/java/top/kgame/lib/test/snapshot/struct/TestSyncComponent.java
+++ src/test/java/top/kgame/lib/test/snapshot/struct/TestSyncComponent.java
@@ -5,6 +5,8 @@ import top.kgame.lib.snapshot.SerializeComponent;
 import top.kgame.lib.snapshot.tools.ReplicatedReader;
 import top.kgame.lib.snapshot.tools.ReplicatedWriter;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class TestSyncComponent implements SerializeComponent, DeserializeComponent {
@@ -19,9 +21,31 @@ public class TestSyncComponent implements SerializeComponent, DeserializeCompone
     private byte[] bs;
     private String ss;
     private TestSyncStruct struct;
+    // List 类型
+    private List<TestSyncStruct> listObj;
+    private List<Boolean> listBoolean;
+    private List<Byte> listByte;
+    private List<Character> listChar;
+    private List<Short> listShort;
+    private List<Integer> listInt;
+    private List<Long> listLong;
+    private List<Float> listFloat;
+    private List<Double> listDouble;
+    private List<String> listString;
+    
+    // 数组类型
+    private boolean[] booleanArray;
+    private char[] charArray;
+    private short[] shortArray;
+    private int[] intArray;
+    private long[] longArray;
+    private float[] floatArray;
+    private double[] doubleArray;
+    private String[] stringArray;
 
     @Override
     public void serialize(ReplicatedWriter writer) {
+        // 基础类型
         writer.writeByte(b);
         writer.writeChar(c);
         writer.writeBoolean(bl);
@@ -32,17 +56,34 @@ public class TestSyncComponent implements SerializeComponent, DeserializeCompone
         writer.writeDouble(f64);
         writer.writeByteArray(bs);
         writer.writeString(ss);
-        if (struct == null) {
-            writer.writeInteger(-1);
-        } else {
-            writer.writeInteger(1);
-            struct.serialize(writer);
-        }
-
+        writer.writeObject(struct);
+        
+        // List 类型
+        writer.writeObjList(listObj);
+        writer.writeBooleanList(listBoolean);
+        writer.writeByteList(listByte);
+        writer.writeCharList(listChar);
+        writer.writeShortList(listShort);
+        writer.writeIntList(listInt);
+        writer.writeLongList(listLong);
+        writer.writeFloatList(listFloat);
+        writer.writeDoubleList(listDouble);
+        writer.writeStringList(listString);
+        
+        // 数组类型
+        writer.writeBooleanArray(booleanArray);
+        writer.writeCharArray(charArray);
+        writer.writeShortArray(shortArray);
+        writer.writeIntArray(intArray);
+        writer.writeLongArray(longArray);
+        writer.writeFloatArray(floatArray);
+        writer.writeDoubleArray(doubleArray);
+        writer.writeStringArray(stringArray);
     }
 
     @Override
     public void deserialize(ReplicatedReader reader) {
+        // 基础类型
         b = reader.readByte();
         c = reader.readChar();
         bl = reader.readBoolean();
@@ -53,13 +94,29 @@ public class TestSyncComponent implements SerializeComponent, DeserializeCompone
         f64 = reader.readDouble();
         bs = reader.readByteArray();
         ss = reader.readString();
-        int objNullType = reader.readInteger();
-        if (objNullType < 0) {
-            struct = null;
-        } else {
-            struct = new TestSyncStruct();
-            struct.deserialize(reader);
-        }
+        struct = reader.readObject(TestSyncStruct.class);
+        
+        // List 类型
+        listObj = reader.readObjList(TestSyncStruct.class);
+        listBoolean = reader.readBooleanList();
+        listByte = reader.readByteList();
+        listChar = reader.readCharList();
+        listShort = reader.readShortList();
+        listInt = reader.readIntList();
+        listLong = reader.readLongList();
+        listFloat = reader.readFloatList();
+        listDouble = reader.readDoubleList();
+        listString = reader.readStringList();
+        
+        // 数组类型
+        booleanArray = reader.readBooleanArray();
+        charArray = reader.readCharArray();
+        shortArray = reader.readShortArray();
+        intArray = reader.readIntArray();
+        longArray = reader.readLongArray();
+        floatArray = reader.readFloatArray();
+        doubleArray = reader.readDoubleArray();
+        stringArray = reader.readStringArray();
     }
 
     @Override
@@ -155,15 +212,201 @@ public class TestSyncComponent implements SerializeComponent, DeserializeCompone
         this.struct = struct;
     }
 
+    // List 类型的 getters 和 setters
+    public List<TestSyncStruct> getListObj() {
+        return listObj;
+    }
+
+    public void setListObj(List<TestSyncStruct> listObj) {
+        this.listObj = listObj;
+    }
+
+    public List<Boolean> getListBoolean() {
+        return listBoolean;
+    }
+
+    public void setListBoolean(List<Boolean> listBoolean) {
+        this.listBoolean = listBoolean;
+    }
+
+    public List<Byte> getListByte() {
+        return listByte;
+    }
+
+    public void setListByte(List<Byte> listByte) {
+        this.listByte = listByte;
+    }
+
+    public List<Character> getListChar() {
+        return listChar;
+    }
+
+    public void setListChar(List<Character> listChar) {
+        this.listChar = listChar;
+    }
+
+    public List<Short> getListShort() {
+        return listShort;
+    }
+
+    public void setListShort(List<Short> listShort) {
+        this.listShort = listShort;
+    }
+
+    public List<Integer> getListInt() {
+        return listInt;
+    }
+
+    public void setListInt(List<Integer> listInt) {
+        this.listInt = listInt;
+    }
+
+    public List<Long> getListLong() {
+        return listLong;
+    }
+
+    public void setListLong(List<Long> listLong) {
+        this.listLong = listLong;
+    }
+
+    public List<Float> getListFloat() {
+        return listFloat;
+    }
+
+    public void setListFloat(List<Float> listFloat) {
+        this.listFloat = listFloat;
+    }
+
+    public List<Double> getListDouble() {
+        return listDouble;
+    }
+
+    public void setListDouble(List<Double> listDouble) {
+        this.listDouble = listDouble;
+    }
+
+    public List<String> getListString() {
+        return listString;
+    }
+
+    public void setListString(List<String> listString) {
+        this.listString = listString;
+    }
+
+    // 数组类型的 getters 和 setters
+    public boolean[] getBooleanArray() {
+        return booleanArray;
+    }
+
+    public void setBooleanArray(boolean[] booleanArray) {
+        this.booleanArray = booleanArray;
+    }
+
+    public char[] getCharArray() {
+        return charArray;
+    }
+
+    public void setCharArray(char[] charArray) {
+        this.charArray = charArray;
+    }
+
+    public short[] getShortArray() {
+        return shortArray;
+    }
+
+    public void setShortArray(short[] shortArray) {
+        this.shortArray = shortArray;
+    }
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public long[] getLongArray() {
+        return longArray;
+    }
+
+    public void setLongArray(long[] longArray) {
+        this.longArray = longArray;
+    }
+
+    public float[] getFloatArray() {
+        return floatArray;
+    }
+
+    public void setFloatArray(float[] floatArray) {
+        this.floatArray = floatArray;
+    }
+
+    public double[] getDoubleArray() {
+        return doubleArray;
+    }
+
+    public void setDoubleArray(double[] doubleArray) {
+        this.doubleArray = doubleArray;
+    }
+
+    public String[] getStringArray() {
+        return stringArray;
+    }
+
+    public void setStringArray(String[] stringArray) {
+        this.stringArray = stringArray;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         TestSyncComponent that = (TestSyncComponent) o;
-        return b == that.b && c == that.c && bl == that.bl && int16 == that.int16 && int32 == that.int32 && int64 == that.int64 && Float.compare(f32, that.f32) == 0 && Double.compare(f64, that.f64) == 0 && Objects.deepEquals(bs, that.bs) && Objects.equals(ss, that.ss) && Objects.equals(struct, that.struct);
+        return b == that.b
+                && c == that.c
+                && bl == that.bl
+                && int16 == that.int16
+                && int32 == that.int32
+                && int64 == that.int64
+                && Float.compare(f32, that.f32) == 0
+                && Double.compare(f64, that.f64) == 0
+                && Objects.deepEquals(bs, that.bs)
+                && Objects.equals(ss, that.ss)
+                && Objects.equals(struct, that.struct)
+                && Objects.equals(listObj, that.listObj)
+                && Objects.equals(listBoolean, that.listBoolean)
+                && Objects.equals(listByte, that.listByte)
+                && Objects.equals(listChar, that.listChar)
+                && Objects.equals(listShort, that.listShort)
+                && Objects.equals(listInt, that.listInt)
+                && Objects.equals(listLong, that.listLong)
+                && Objects.equals(listFloat, that.listFloat)
+                && Objects.equals(listDouble, that.listDouble)
+                && Objects.equals(listString, that.listString)
+                && Objects.deepEquals(booleanArray, that.booleanArray)
+                && Objects.deepEquals(charArray, that.charArray)
+                && Objects.deepEquals(shortArray, that.shortArray)
+                && Objects.deepEquals(intArray, that.intArray)
+                && Objects.deepEquals(longArray, that.longArray)
+                && Objects.deepEquals(floatArray, that.floatArray)
+                && Objects.deepEquals(doubleArray, that.doubleArray)
+                && Objects.deepEquals(stringArray, that.stringArray);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(b, c, bl, int16, int32, int64);
+        int result = Objects.hash(b, c, bl, int16, int32, int64, f32, f64, ss, struct,
+                                  listObj, listBoolean, listByte, listChar, listShort, 
+                                  listInt, listLong, listFloat, listDouble, listString);
+        result = 31 * result + Arrays.hashCode(bs);
+        result = 31 * result + Arrays.hashCode(booleanArray);
+        result = 31 * result + Arrays.hashCode(charArray);
+        result = 31 * result + Arrays.hashCode(shortArray);
+        result = 31 * result + Arrays.hashCode(intArray);
+        result = 31 * result + Arrays.hashCode(longArray);
+        result = 31 * result + Arrays.hashCode(floatArray);
+        result = 31 * result + Arrays.hashCode(doubleArray);
+        result = 31 * result + Arrays.hashCode(stringArray);
+        return result;
     }
 }
